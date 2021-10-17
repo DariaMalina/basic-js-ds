f77f4ed40f4c079c3a4d24532deccba7d43d90cf
Daria Malina <gluto68@gmail.com>
2021-10-17 16:59:07 +0300

    feat: add LL in task

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
  const {ListNode} = require('../extensions/list-node.js');
+ const {LinkedList} = require("./LinkedList.js");
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,37 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class LL {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
 
-  addTailNode(value) {
-    if (!this.head) {
-      this.head = new ListNode(value)
-    }
-
-    if (this.tail) {
-      this.tail.next = new ListNode(value)
-      this.tail = this.tail.next
-    } else {
-      this.tail = this.head
-    }
-  }
-
-  removeHeadNode() {
-    let currentValue = this.head.value
-    let current = this.head
-    this.head = current.next
-    return currentValue
-  }
-}
 
 module.exports = class Queue {
 
   constructor() {
-    this.list = new LL()
+    this.list = new LinkedList()
   }
 
   getUnderlyingList() {
