bfe83fb4b3e3c97d52113dbe29423d76c3a856f0
Daria Malina <gluto68@gmail.com>
2021-10-18 15:21:36 +0300

    feat: add solution BST task1

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,39 +1,55 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
+// Node = class {
+//     constructor(data) {
+//         this.data = data;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
 module.exports = class BinarySearchTree {
+  _min = null;
+  _max = null;
+  data = null;
 
   root() {
-
+    return this.data;
   }
 
-  add( data ) {
-
+  add(data) {
+    if (this._max < data) {
+      this._max = data;
+      this._min = data;
+    }
+    if (this._min > data) {
+      this._min = data;
+    }
+    if (!this.data) {
+      this.data = new Node(data);
+    }
   }
 
-  has( data ) {
-
+  has(data) {
+    return false;
   }
 
-  find( data ) {
-
+  find(data) {
+    return null;
   }
 
-  remove( data ) {
-
-  }
+  remove(data) {}
 
   min() {
-
+    return this._min;
   }
 
   max() {
-
+    return this._max;
   }
-
-}
+};
