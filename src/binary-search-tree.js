344717841f5ae8abd3d2b94396bfa371b99b4e51
Daria Malina <gluto68@gmail.com>
2021-10-18 21:10:02 +0300

    feat: add partially solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -14,42 +14,99 @@ const { Node } = require("../extensions/list-tree.js");
 //     }
 // }
 module.exports = class BinarySearchTree {
-  _min = null;
-  _max = null;
-  data = null;
+  constructor() {
+    this._root = null;
+  }
+
+  #deepAdd(curr, newNode) {
+    if (curr.data < newNode.data) {
+      if (curr.right === null) {
+        curr.right = newNode;
+      } else {
+        this.#deepAdd(curr.right, newNode);
+      }
+    }
+    if (curr.data > newNode.data) {
+      if (curr.left === null) {
+        curr.left = newNode;
+      } else {
+        this.#deepAdd(curr.left, newNode);
+      }
+    }
+  }
+
+  #searchDate(node, date) {
+    if (node === null) {
+      return false;
+    } else if (node.data > date) {
+      return this.#searchDate(node.left, date);
+    } else if (node.data < date) {
+      return this.#searchDate(node.right, date);
+    } else if (node.data === date) {
+      return true;
+    }
+  }
+
+  #findDate(node, date) {
+    if (node === null) {
+      return null;
+    } else if (node.data > date) {
+      return this.#findDate(node.left, date);
+    } else if (node.data < date) {
+      return this.#findDate(node.right, date);
+    } else if (node.data === date) {
+      return node;
+    }
+  }
+
+  #removeDate(node, data) {}
 
   root() {
-    return this.data;
+    return this._root;
   }
 
   add(data) {
-    if (this._max < data) {
-      this._max = data;
-      this._min = data;
-    }
-    if (this._min > data) {
-      this._min = data;
-    }
-    if (!this.data) {
-      this.data = new Node(data);
+    let newNode = new Node(data);
+    if (!this._root) {
+      this._root = new Node(data);
+    } else {
+      this.#deepAdd(this._root, newNode);
     }
   }
 
   has(data) {
-    return false;
+    return this.#searchDate(this._root, data);
   }
 
   find(data) {
-    return null;
+    return this.#findDate(this._root, data);
   }
 
-  remove(data) {}
+  remove(data) {
+    if (this._root.data === data) {
+      this._root = null;
+    } else {
+      this.#removeDate(this._root, data);
+    }
+  }
 
   min() {
-    return this._min;
+    let minLeft = this._root.left;
+    let min;
+    while (minLeft !== null) {
+      min = minLeft.data;
+      minLeft = minLeft.left;
+    }
+    return min;
   }
 
   max() {
-    return this._max;
+    let minRight = this._root.right;
+    let max;
+    while (minRight !== null) {
+      max = minRight.data;
+      minRight = minRight.right;
+    }
+    return max;
   }
 };
